ac7185c6aeca1a3275ea5ca74b9a3201e0c89865
CCliftS <90357585+CCliftS@users.noreply.github.com>
2024-06-16 22:04:33 -0400

    Update GO Services

--- pkg/proto/product_grpc.pb.go
+++ pkg/proto/product_grpc.pb.go
@@ -19,9 +19,14 @@ import (
 const _ = grpc.SupportPackageIsVersion8
 
 const (
-	ProductService_CreateProduct_FullMethodName = "/product.ProductService/CreateProduct"
-	ProductService_FindOne_FullMethodName       = "/product.ProductService/FindOne"
-	ProductService_DecreaseStock_FullMethodName = "/product.ProductService/DecreaseStock"
+	ProductService_CreateProduct_FullMethodName  = "/product.ProductService/CreateProduct"
+	ProductService_DecreaseStock_FullMethodName  = "/product.ProductService/DecreaseStock"
+	ProductService_FindOne_FullMethodName        = "/product.ProductService/FindOne"
+	ProductService_FindAll_FullMethodName        = "/product.ProductService/FindAll"
+	ProductService_FindByName_FullMethodName     = "/product.ProductService/FindByName"
+	ProductService_FindByCategory_FullMethodName = "/product.ProductService/FindByCategory"
+	ProductService_DeleteProduct_FullMethodName  = "/product.ProductService/DeleteProduct"
+	ProductService_UpdateProduct_FullMethodName  = "/product.ProductService/UpdateProduct"
 )
 
 // ProductServiceClient is the client API for ProductService service.
@@ -29,8 +34,13 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ProductServiceClient interface {
 	CreateProduct(ctx context.Context, in *CreateProductRequest, opts ...grpc.CallOption) (*CreateProductResponse, error)
-	FindOne(ctx context.Context, in *FindOneRequest, opts ...grpc.CallOption) (*FindOneResponse, error)
 	DecreaseStock(ctx context.Context, in *DecreaseStockRequest, opts ...grpc.CallOption) (*DecreaseStockResponse, error)
+	FindOne(ctx context.Context, in *FindOneRequest, opts ...grpc.CallOption) (*FindOneResponse, error)
+	FindAll(ctx context.Context, in *FindAllRequest, opts ...grpc.CallOption) (*FindAllResponse, error)
+	FindByName(ctx context.Context, in *FindByNameRequest, opts ...grpc.CallOption) (*FindByNameResponse, error)
+	FindByCategory(ctx context.Context, in *FindByCategoryRequest, opts ...grpc.CallOption) (*FindByCategoryResponse, error)
+	DeleteProduct(ctx context.Context, in *DeleteProductRequest, opts ...grpc.CallOption) (*DeleteProductResponse, error)
+	UpdateProduct(ctx context.Context, in *UpdateProductRequest, opts ...grpc.CallOption) (*UpdateProductResponse, error)
 }
 
 type productServiceClient struct {
@@ -51,6 +61,16 @@ func (c *productServiceClient) CreateProduct(ctx context.Context, in *CreateProd
 	return out, nil
 }
 
+func (c *productServiceClient) DecreaseStock(ctx context.Context, in *DecreaseStockRequest, opts ...grpc.CallOption) (*DecreaseStockResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(DecreaseStockResponse)
+	err := c.cc.Invoke(ctx, ProductService_DecreaseStock_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *productServiceClient) FindOne(ctx context.Context, in *FindOneRequest, opts ...grpc.CallOption) (*FindOneResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(FindOneResponse)
@@ -61,10 +81,50 @@ func (c *productServiceClient) FindOne(ctx context.Context, in *FindOneRequest, 
 	return out, nil
 }
 
-func (c *productServiceClient) DecreaseStock(ctx context.Context, in *DecreaseStockRequest, opts ...grpc.CallOption) (*DecreaseStockResponse, error) {
+func (c *productServiceClient) FindAll(ctx context.Context, in *FindAllRequest, opts ...grpc.CallOption) (*FindAllResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(DecreaseStockResponse)
-	err := c.cc.Invoke(ctx, ProductService_DecreaseStock_FullMethodName, in, out, cOpts...)
+	out := new(FindAllResponse)
+	err := c.cc.Invoke(ctx, ProductService_FindAll_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *productServiceClient) FindByName(ctx context.Context, in *FindByNameRequest, opts ...grpc.CallOption) (*FindByNameResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(FindByNameResponse)
+	err := c.cc.Invoke(ctx, ProductService_FindByName_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *productServiceClient) FindByCategory(ctx context.Context, in *FindByCategoryRequest, opts ...grpc.CallOption) (*FindByCategoryResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(FindByCategoryResponse)
+	err := c.cc.Invoke(ctx, ProductService_FindByCategory_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *productServiceClient) DeleteProduct(ctx context.Context, in *DeleteProductRequest, opts ...grpc.CallOption) (*DeleteProductResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(DeleteProductResponse)
+	err := c.cc.Invoke(ctx, ProductService_DeleteProduct_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *productServiceClient) UpdateProduct(ctx context.Context, in *UpdateProductRequest, opts ...grpc.CallOption) (*UpdateProductResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(UpdateProductResponse)
+	err := c.cc.Invoke(ctx, ProductService_UpdateProduct_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
@@ -76,8 +136,13 @@ func (c *productServiceClient) DecreaseStock(ctx context.Context, in *DecreaseSt
 // for forward compatibility
 type ProductServiceServer interface {
 	CreateProduct(context.Context, *CreateProductRequest) (*CreateProductResponse, error)
-	FindOne(context.Context, *FindOneRequest) (*FindOneResponse, error)
 	DecreaseStock(context.Context, *DecreaseStockRequest) (*DecreaseStockResponse, error)
+	FindOne(context.Context, *FindOneRequest) (*FindOneResponse, error)
+	FindAll(context.Context, *FindAllRequest) (*FindAllResponse, error)
+	FindByName(context.Context, *FindByNameRequest) (*FindByNameResponse, error)
+	FindByCategory(context.Context, *FindByCategoryRequest) (*FindByCategoryResponse, error)
+	DeleteProduct(context.Context, *DeleteProductRequest) (*DeleteProductResponse, error)
+	UpdateProduct(context.Context, *UpdateProductRequest) (*UpdateProductResponse, error)
 	mustEmbedUnimplementedProductServiceServer()
 }
 
@@ -88,11 +153,26 @@ type UnimplementedProductServiceServer struct {
 func (UnimplementedProductServiceServer) CreateProduct(context.Context, *CreateProductRequest) (*CreateProductResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateProduct not implemented")
 }
+func (UnimplementedProductServiceServer) DecreaseStock(context.Context, *DecreaseStockRequest) (*DecreaseStockResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DecreaseStock not implemented")
+}
 func (UnimplementedProductServiceServer) FindOne(context.Context, *FindOneRequest) (*FindOneResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method FindOne not implemented")
 }
-func (UnimplementedProductServiceServer) DecreaseStock(context.Context, *DecreaseStockRequest) (*DecreaseStockResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DecreaseStock not implemented")
+func (UnimplementedProductServiceServer) FindAll(context.Context, *FindAllRequest) (*FindAllResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindAll not implemented")
+}
+func (UnimplementedProductServiceServer) FindByName(context.Context, *FindByNameRequest) (*FindByNameResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindByName not implemented")
+}
+func (UnimplementedProductServiceServer) FindByCategory(context.Context, *FindByCategoryRequest) (*FindByCategoryResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method FindByCategory not implemented")
+}
+func (UnimplementedProductServiceServer) DeleteProduct(context.Context, *DeleteProductRequest) (*DeleteProductResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteProduct not implemented")
+}
+func (UnimplementedProductServiceServer) UpdateProduct(context.Context, *UpdateProductRequest) (*UpdateProductResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateProduct not implemented")
 }
 func (UnimplementedProductServiceServer) mustEmbedUnimplementedProductServiceServer() {}
 
@@ -125,24 +205,6 @@ func _ProductService_CreateProduct_Handler(srv interface{}, ctx context.Context,
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ProductService_FindOne_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(FindOneRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(ProductServiceServer).FindOne(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: ProductService_FindOne_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ProductServiceServer).FindOne(ctx, req.(*FindOneRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 func _ProductService_DecreaseStock_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(DecreaseStockRequest)
 	if err := dec(in); err != nil {
@@ -161,6 +223,114 @@ func _ProductService_DecreaseStock_Handler(srv interface{}, ctx context.Context,
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ProductService_FindOne_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FindOneRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductServiceServer).FindOne(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ProductService_FindOne_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductServiceServer).FindOne(ctx, req.(*FindOneRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProductService_FindAll_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FindAllRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductServiceServer).FindAll(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ProductService_FindAll_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductServiceServer).FindAll(ctx, req.(*FindAllRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProductService_FindByName_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FindByNameRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductServiceServer).FindByName(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ProductService_FindByName_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductServiceServer).FindByName(ctx, req.(*FindByNameRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProductService_FindByCategory_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FindByCategoryRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductServiceServer).FindByCategory(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ProductService_FindByCategory_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductServiceServer).FindByCategory(ctx, req.(*FindByCategoryRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProductService_DeleteProduct_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteProductRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductServiceServer).DeleteProduct(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ProductService_DeleteProduct_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductServiceServer).DeleteProduct(ctx, req.(*DeleteProductRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProductService_UpdateProduct_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateProductRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductServiceServer).UpdateProduct(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ProductService_UpdateProduct_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductServiceServer).UpdateProduct(ctx, req.(*UpdateProductRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ProductService_ServiceDesc is the grpc.ServiceDesc for ProductService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -173,12 +343,32 @@ var ProductService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _ProductService_CreateProduct_Handler,
 		},
 		{
+			MethodName: "DecreaseStock",
+			Handler:    _ProductService_DecreaseStock_Handler,
+		},
+		{
 			MethodName: "FindOne",
 			Handler:    _ProductService_FindOne_Handler,
 		},
 		{
-			MethodName: "DecreaseStock",
-			Handler:    _ProductService_DecreaseStock_Handler,
+			MethodName: "FindAll",
+			Handler:    _ProductService_FindAll_Handler,
+		},
+		{
+			MethodName: "FindByName",
+			Handler:    _ProductService_FindByName_Handler,
+		},
+		{
+			MethodName: "FindByCategory",
+			Handler:    _ProductService_FindByCategory_Handler,
+		},
+		{
+			MethodName: "DeleteProduct",
+			Handler:    _ProductService_DeleteProduct_Handler,
+		},
+		{
+			MethodName: "UpdateProduct",
+			Handler:    _ProductService_UpdateProduct_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- pkg/services/product.go
+++ pkg/services/product.go
@@ -104,3 +104,134 @@ func (s *Server) DecreaseStock(ctx context.Context, req *pb.DecreaseStockRequest
 		Status: http.StatusOK,
 	}, nil
 }
+
+func (s *Server) UpdateProduct(ctx context.Context, req *pb.UpdateProductRequest) (*pb.UpdateProductResponse, error) {
+	var product models.Product
+
+	if result := s.H.DB.First(&product, req.ProductId); result.Error != nil {
+		return &pb.UpdateProductResponse{
+			Status: http.StatusNotFound,
+			Error:  []string{result.Error.Error()},
+		}, nil
+	}
+
+	product.Name = req.Product.Name
+	product.Sku = req.Product.Sku
+	product.Category = req.Product.Category
+	product.Description = req.Product.Description
+	product.Stock = req.Product.Stock
+	product.Price = req.Product.Price
+
+	s.H.DB.Save(&product)
+
+	return &pb.UpdateProductResponse{
+		Status: http.StatusOK,
+	}, nil
+}
+
+func (s *Server) FindByCategory(ctx context.Context, req *pb.FindByCategoryRequest) (*pb.FindByCategoryResponse, error) {
+	var products []models.Product
+
+	if result := s.H.DB.Where("category = ?", req.Category).Find(&products); result.Error != nil {
+		return &pb.FindByCategoryResponse{
+			Status: http.StatusNotFound,
+			Error:  []string{result.Error.Error()},
+		}, nil
+	}
+
+	var data []*pb.Product
+
+	for _, product := range products {
+		data = append(data, &pb.Product{
+			Id:          product.Id,
+			Name:        product.Name,
+			Sku:         product.Sku,
+			Category:    product.Category,
+			Description: product.Description,
+			Stock:       product.Stock,
+			Price:       product.Price,
+		})
+	}
+
+	return &pb.FindByCategoryResponse{
+		Status:   http.StatusOK,
+		Products: data,
+	}, nil
+}
+
+func (s *Server) FindByName(ctx context.Context, req *pb.FindByNameRequest) (*pb.FindByNameResponse, error) {
+	var products []models.Product
+
+	if result := s.H.DB.Where("name = ?", req.Name).Find(&products); result.Error != nil {
+		return &pb.FindByNameResponse{
+			Status: http.StatusNotFound,
+			Error:  []string{result.Error.Error()},
+		}, nil
+	}
+
+	var data []*pb.Product
+
+	for _, product := range products {
+		data = append(data, &pb.Product{
+			Id:          product.Id,
+			Name:        product.Name,
+			Sku:         product.Sku,
+			Category:    product.Category,
+			Description: product.Description,
+			Stock:       product.Stock,
+			Price:       product.Price,
+		})
+	}
+
+	return &pb.FindByNameResponse{
+		Status:   http.StatusOK,
+		Products: data,
+	}, nil
+}
+
+func (s *Server) FindAll(ctx context.Context, req *pb.FindAllRequest) (*pb.FindAllResponse, error) {
+	var products []models.Product
+
+	if result := s.H.DB.Find(&products); result.Error != nil {
+		return &pb.FindAllResponse{
+			Status: http.StatusNotFound,
+			Error:  []string{result.Error.Error()},
+		}, nil
+	}
+
+	var data []*pb.Product
+
+	for _, product := range products {
+		data = append(data, &pb.Product{
+			Id:          product.Id,
+			Name:        product.Name,
+			Sku:         product.Sku,
+			Category:    product.Category,
+			Description: product.Description,
+			Stock:       product.Stock,
+			Price:       product.Price,
+		})
+	}
+
+	return &pb.FindAllResponse{
+		Status:   http.StatusOK,
+		Products: data,
+	}, nil
+}
+
+func (s *Server) DeleteProduct(ctx context.Context, req *pb.DeleteProductRequest) (*pb.DeleteProductResponse, error) {
+	var product models.Product
+
+	if result := s.H.DB.First(&product, req.Id); result.Error != nil {
+		return &pb.DeleteProductResponse{
+			Status: http.StatusNotFound,
+			Error:  []string{result.Error.Error()},
+		}, nil
+	}
+
+	s.H.DB.Delete(&product)
+
+	return &pb.DeleteProductResponse{
+		Status: http.StatusOK,
+	}, nil
+}
